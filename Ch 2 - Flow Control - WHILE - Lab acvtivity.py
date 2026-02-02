0a3fc432463ee5a62bcfc7b4135f6a80a1d05964
Julianne-Oviso <jujuoviso@gmail.com>
2026-02-02 14:49:41 -0800

    Flow Control-While Loop-Lab acvtivity.py

---
+++ Ch 2 - Flow Control - WHILE - Lab acvtivity.py
@@ -0,0 +1,9 @@
+while True:
+    x = 12
+    y = 9
+    print("Starting The Code Challenge")
+    print('x =',x,'\ny =',y)
+    z = x + y
+    print('x + y = z')
+    print(' z = ', z)
+    print('Ending The Code Challenge')
