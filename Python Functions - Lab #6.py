b7e3901c2d5cb6196415952b39ae14e1c5181aed
Julianne-Oviso <jujuoviso@gmail.com>
2026-02-05 14:04:46 -0800

    Create Python Functions - Lab #6.py

---
+++ Python Functions - Lab #6.py
@@ -0,0 +1,24 @@
+# Challenge No. 2
+def main():
+    x = 2
+    y = 3
+    answer = x * y
+    return answer
+
+main()
+
+# Challenge No.3
+
+def main():
+    x = 3
+    y = 1
+    z = x + y
+    print(x, "+", y, "=", z)
+    if z % 2 == 0:
+        print("z is an even number")
+        value = z + 1
+        print(z, "+", "1", "=", value)
+    else:
+        return z
+
+main() 
