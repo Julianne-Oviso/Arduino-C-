a9edc60b707c33ee03f3757fe8bc9cd8328c28cb
Julianne-Oviso <jujuoviso@gmail.com>
2026-02-06 13:57:08 -0800

    Create While Loop - Print a Count Value.py

---
+++ While Loop - Print a Count Value.py
@@ -0,0 +1,9 @@
+def main():
+    print("Starting While Loop - Print Count Variable")
+    counter = 0 
+    while counter < 10:
+        print(counter)
+        counter += 1
+    print("Ending While Loop")
+
+main() 
