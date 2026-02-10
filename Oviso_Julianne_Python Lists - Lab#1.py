07368692ce7f55469daae78a43c396008a5e051e
Julianne-Oviso <jujuoviso@gmail.com>
2026-02-10 15:32:54 -0800

    Create Oviso_Julianne_Python Lists - Lab#1.py

---
+++ Oviso_Julianne_Python Lists - Lab#1.py
@@ -0,0 +1,28 @@
+# Challenge No. 1
+x = [1,3,5,23,9,14,42]
+print(x)
+print(type(x))
+
+# Challenge No. 2
+x = ['4juju','jUju', 12, True, 9.3]
+print('x is now a list of mixed objects')
+print(x)
+print(type(x))
+
+# Challenge No. 3
+x = ['4juju','jUju', 12, True, 9.3,'R0yce',566,'hello']
+print('x is now a list of mixed objects')
+print(x)
+print(type(x))
+
+# Challenge No. 4
+x = ['4juju','jUju', 12, True, 9.3,'R0yce',566,'hello']
+print('x is now a list of mixed objects')
+print(x)
+print(type(x))
+print("Let's Index the list")
+print(x[4])
+print(type(x[4]))
+y = x[2]
+print(y)
+print(type(y)) 
